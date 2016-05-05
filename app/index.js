1d281cfa7713fdff59dcba6b4cf24d7a114cf66c
alanbares <neloice@gmail.com>
2016-05-04 21:05:12 -0700

    Add custom Link component and properties

--- app/index.js
+++ app/index.js
@@ -7,34 +7,49 @@ var USER_DATA = {
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+var Link = React.createClass({
+  changeURL: function () {
+    window.location.replace(this.props.href)
+  },
+  render: function () {
+    return (
+      <span
+        style={{color: 'blue', cursor: 'pointer'}}
+        onClick={this.changeURL}>
+        {this.props.children}
+      </span>
+    )
+  }
+});
+
 var ProfilePic = React.createClass({
-  render: function() {
+  render: function () {
     return <img src={this.props.imageUrl} style={{height: 100, width: 100}} />
   }
 });
 
 var ProfileLink = React.createClass({
-  render: function() {
+  render: function () {
     return (
       <div>
-        <a href={"https://www.github.com/" + this.props.username}>
+        <Link href={"https://www.github.com/" + this.props.username}>
           {this.props.username}
-        </a>
+        </Link>
       </div>
     )
   }
 });
 
 var ProfileName = React.createClass({
-  render: function() {
-    return(
+  render: function () {
+    return (
       <div>{this.props.name}</div>
     )
   }
 });
 
 var Avatar = React.createClass({
-    render: function() {
+    render: function () {
       return (
         <div>
           <ProfilePic imageUrl={this.props.user.image} />
