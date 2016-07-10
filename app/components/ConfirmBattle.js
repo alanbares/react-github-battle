86d196e964f6959651b24438a2ecba9b44dc5e29
alanbares <neloice@gmail.com>
2016-07-09 21:41:48 -0700

    Add user detail wrapper component and refactor ui in confirm battle component with wrapper. Add spacing between buttons in confirm battle component

--- app/components/ConfirmBattle.js
+++ app/components/ConfirmBattle.js
@@ -1,26 +1,22 @@
 var React = require('react');
 var PropTypes = React.PropTypes;
-var styles = require('../styles')
-var Link = require('react-router').Link
-var UserDetails = require('./UserDetails')
+var styles = require('../styles');
+var Link = require('react-router').Link;
+var UserDetails = require('./UserDetails');
+var UserDetailsWrapper = require('./UserDetailsWrapper');
 
-function puke (object) {
-  return <pre>{JSON.stringify(object, null, ' ')}</pre>;
-}
 function ConfirmBattle(props) {
   return props.isLoading === true
     ? <p>LOADING</p>
     : <div className="jumbotron col-sm-12 text-center" style={styles.transparentBg}>
         <h1>Confirm Players</h1>
         <div className='col-sm-8 col-sm-offset-2'>
-          <div className='col-sm-6'>
-            <p className='lead'>Player 1</p>
-              <UserDetails info={props.playersInfo[0]} />
-          </div>
-          <div className='col-sm-6'>
-            <p className='lead'>Player 2</p>
+          <UserDetailsWrapper header='Player One'>
+            <UserDetails info={props.playersInfo[0]} />
+          </UserDetailsWrapper>
+          <UserDetailsWrapper header='Player Two'>
             <UserDetails info={props.playersInfo[1]} />
-          </div>
+          </UserDetailsWrapper>
         </div>
         <div className='col-sm-8 col-sm-offset-2'>
           <div className='col-sm-12' style={styles.space}>
